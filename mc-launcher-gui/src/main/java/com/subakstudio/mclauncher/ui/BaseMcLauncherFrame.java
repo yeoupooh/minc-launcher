427df6e1b37c0119ccf10d98085df12746bbda16
Thomas Min <thomas.min.git@gmail.com>
2016-01-03 13:21:27 +0900

    Add base frame.

---
+++ mc-launcher-gui/src/main/java/com/subakstudio/mclauncher/ui/BaseMcLauncherFrame.java
@@ -0,0 +1,36 @@
+package com.subakstudio.mclauncher.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ResourceBundle;
+
+/**
+ * Created by yeoupooh on 1/3/16.
+ */
+public abstract class BaseMcLauncherFrame extends JFrame implements ActionListener {
+
+    private ActionListener actionListener;
+
+    public abstract Container getRootContentPane();
+
+    public void setupUI() {
+        setContentPane(getRootContentPane());
+        setTitle(ResourceBundle.getBundle("strings").getString("app.name"));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(800, 600);
+    }
+
+    public void setActionListener(ActionListener actionListener) {
+        this.actionListener = actionListener;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (actionListener != null) {
+            actionListener.actionPerformed(e);
+        }
+    }
+}
