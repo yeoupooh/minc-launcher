c365b21957c9a03d34fd356222c2faad4a75a8db
Thomas Min <thomas.min.git@gmail.com>
2016-04-20 18:01:32 +0900

    Add DownloadFileServiceTest with TestFX

---
+++ mc-launcher-fx/src/test/java/com/subakstudio/mclauncher/DownloadFileServiceTest.java
@@ -0,0 +1,28 @@
+package com.subakstudio.mclauncher;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yeoupooh on 4/20/16.
+ */
+public class DownloadFileServiceTest extends ApplicationTest {
+    @Test
+    public void setUrl() throws Exception {
+        DownloadFileService service = new DownloadFileService(() -> System.out.println("completed"));
+        service.setUrl("http://www.google.com/");
+        service.start();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(new Pane(), 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
