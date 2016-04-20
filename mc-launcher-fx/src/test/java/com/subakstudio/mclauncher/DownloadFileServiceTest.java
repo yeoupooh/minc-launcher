3574bb34d822f7d3bddf2b11ed4f1d6a05107ac1
Thomas Min <thomas.min.git@gmail.com>
2016-04-20 18:50:49 +0900

    Ignore ui test for travis.

--- mc-launcher-fx/src/test/java/com/subakstudio/mclauncher/DownloadFileServiceTest.java
+++ mc-launcher-fx/src/test/java/com/subakstudio/mclauncher/DownloadFileServiceTest.java
@@ -3,6 +3,7 @@ package com.subakstudio.mclauncher;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
@@ -12,6 +13,8 @@ import static org.junit.Assert.*;
  * Created by yeoupooh on 4/20/16.
  */
 public class DownloadFileServiceTest extends ApplicationTest {
+    // NOTE It hangs on travis. So run this only in local.
+    @Ignore
     @Test
     public void setUrl() throws Exception {
         DownloadFileService service = new DownloadFileService(() -> System.out.println("completed"));
